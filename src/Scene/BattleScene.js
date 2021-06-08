5c4bf1262c9919f7630e3bd91b14061148e8b5a7
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-06-08 17:51:24 +0200

    Add config data

--- src/Scene/BattleScene.js
+++ src/Scene/BattleScene.js
@@ -1,5 +1,44 @@
 import Phaser from 'phaser';
 
+// the message class extends containter
+const Message = new Phaser.Class({
+
+  Extends: Phaser.GameObjects.Container,
+
+  initialize:
+  function Message(scene, events) {
+    Phaser.GameObjects.Container.call(this, scene, 160, 30);
+    const graphics = this.scene.add.graphics();
+    this.add(graphics);
+    graphics.lineStyle(1, 0xffffff, 0.8);
+    graphics.fillStyle(0x031f4c, 0.3);
+    graphics.strokeRect(-90, -15, 180, 30);
+    graphics.fillRect(-90, -15, 180, 30);
+    this.text = new Phaser.GameObjects.Text(scene, 0, 0, '', {
+      color: '#ffffff', align: 'center', fontSize: 13, wordWrap: { width: 170, useAdvancedWrap: true },
+    });
+    this.add(this.text);
+    this.text.setOrigin(0.5);
+    events.on('Message', this.showMessage, this);
+    this.visible = false;
+  },
+  showMessage(text) {
+    this.text.setText(text);
+    this.visible = true;
+    if (this.hideEvent) {
+      this.hideEvent.remove(false);
+      this.hideEvent = this.scene.time.addEvent({
+        delay: 2000, callback: this.hideMessage, callbackScope: this,
+      });
+    }
+  },
+
+  hideMessage() {
+    this.hideEvent = null;
+    this.visible = false;
+  },
+});
+
 // base class for heroes and enemies
 const Unit = new Phaser.Class({
   Extends: Phaser.GameObjects.Sprite,
@@ -449,45 +488,6 @@ const UIScene = new Phaser.Class({
         this.currentMenu.confirm();
       }
     }
-  },
-});
-
-// the message class extends containter
-const Message = new Phaser.Class({
-
-  Extends: Phaser.GameObjects.Container,
-
-  initialize:
-  function Message(scene, events) {
-    Phaser.GameObjects.Container.call(this, scene, 160, 30);
-    const graphics = this.scene.add.graphics();
-    this.add(graphics);
-    graphics.lineStyle(1, 0xffffff, 0.8);
-    graphics.fillStyle(0x031f4c, 0.3);
-    graphics.strokeRect(-90, -15, 180, 30);
-    graphics.fillRect(-90, -15, 180, 30);
-    this.text = new Phaser.GameObjects.Text(scene, 0, 0, '', {
-      color: '#ffffff', align: 'center', fontSize: 13, wordWrap: { width: 170, useAdvancedWrap: true },
-    });
-    this.add(this.text);
-    this.text.setOrigin(0.5);
-    events.on('Message', this.showMessage, this);
-    this.visible = false;
-  },
-  showMessage(text) {
-    this.text.setText(text);
-    this.visible = true;
-    if (this.hideEvent) {
-      this.hideEvent.remove(false);
-      this.hideEvent = this.scene.time.addEvent({
-        delay: 2000, callback: this.hideMessage, callbackScope: this,
-      });
-    }
-  },
-
-  hideMessage() {
-    this.hideEvent = null;
-    this.visible = false;
   },
 });
 
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
-      debug: true, // set to true to view zones
+      debug: false, // set to true to view zones
     },
   },
   scene: [
