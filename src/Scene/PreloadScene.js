32602ae02231ca970f9e02f4f805afc1b6351cf3
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-06-15 08:53:51 +0200

    Fix preloader UI

--- src/Scene/PreloadScene.js
+++ src/Scene/PreloadScene.js
@@ -6,8 +6,7 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
-    // add logo image
-    this.add.image(400, 200, 'logo');
+    this.add.image(180, 100, 'Logo').setScale(0.5);
 
     // display progress bar
     const progressBar = this.add.graphics();
@@ -15,14 +14,14 @@ export default class PreloaderScene extends Phaser.Scene {
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
 
-    const { width } = this.cameras.main.width;
-    const { height } = this.cameras.main.height;
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
     const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
       text: 'Loading...',
       style: {
-        font: '20px monospace',
+        font: '15px monospace',
         fill: '#ffffff',
       },
     });
@@ -52,24 +51,15 @@ export default class PreloaderScene extends Phaser.Scene {
 
     // update progress bar
     this.load.on('progress', (value) => {
-      percentText.setText(parseInt`(${value * 100}) %`);
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
-    // update file progress text
+    // update file progress
     this.load.on('fileprogress', (file) => {
       assetText.setText(`Loading asset: ${file.key}`);
-    });
-
-    // remove progress bar when complete
-    this.load.on('complete', () => {
-      progressBar.destroy();
-      progressBox.destroy();
-      loadingText.destroy();
-      percentText.destroy();
-      assetText.destroy();
     });
 
     // remove progress bar when complete
